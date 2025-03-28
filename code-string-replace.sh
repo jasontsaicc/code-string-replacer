1222fa862e5d6e12c822bb4ff7408b62c9e48e58
jason tsai <jason.tsaicc@gmail.com>
2025-03-28 17:29:12 +0800

    add CLI argument parsing for find, replace, and exclude options

--- code-string-replace.sh
+++ code-string-replace.sh
@@ -10,6 +10,27 @@ EXCLUDE_PATTERNS=""
 
 # Ensure -find and -replace are not empty
 # Read CLI arguments
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --find)
+            FIND_STRING="$2"
+            shift 2
+            ;;
+        --replace)
+            REPLACE_STRING="$2"
+            shift 2
+            ;;
+        --exclude)
+            IFS=',' read -ra EXC:LUDE_PATTERNS <<< "$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown parameter passed: $1"
+            exit 1
+            ;;
+    esac
+done
+
 
 # 3. Create a counter: replace_count = 0
 
